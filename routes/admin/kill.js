a8edea6433233678975be1b1c44226d2eeef5206
Snazzah <suggesttosnazzy@gmail.com>
2018-05-15 18:21:34 -0500

    fix /admin/kill

--- routes/admin/kill.js
+++ routes/admin/kill.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
+const { Constants } = require('struct')
 
-router.post('/', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   let token = await req.db.valid(req.get('Authorization'))
-  if(req.get('Authorization') !== req.config.masterkey) return res.status(401).send(req.constants.StatusBody.BadAuth)
+  if(req.get('Authorization') !== req.config.masterkey) return res.status(401).send(Constants.StatusBody.BadAuth)
   res.status(200).send({})
   process.exit(0)
 })
