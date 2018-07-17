6ef012326e8e1f6d0a06bdb1b40b723dd23f752e
Snazzah <suggesttosnazzy@gmail.com>
2018-07-17 09:18:47 -0500

    Finish special args in /filter/distort

--- routes/filter/distort.js
+++ routes/filter/distort.js
@@ -13,6 +13,8 @@ router.schema = Joi.object().keys({
   spin: Joi.number().integer().min(0).max(359).optional(),
   implode: Joi.number().integer().min(0).max(10).optional(),
   roll: Joi.boolean().optional(),
+  horizontalRoll: Joi.number().integer().min(-180).max(180).optional(),
+  verticalRoll: Joi.number().integer().min(-180).max(180).optional(),
   swirl: Joi.number().integer().min(-180).max(180).optional()
 })
 
@@ -21,26 +23,32 @@ Util.genericTemplatePost(router)
 router.imageprocess = class distort extends ImageCode {
   async process(msg) {
     let img1 = await Jimp.read(msg.picture)
-    const filters = [
-      { apply: (msg.saturate !== undefined ? msg.saturate : this.rBool()) ? 'desaturate' : 'saturate', params: [msg.saturateAmount || this.rInt(40, 80)] },
-      { apply: 'spin', params: [msg.spin || this.rInt(10, 350)] }
+    let special = Object.keys(msg).length !== 3
+    let filters = special ? [] : [
+      { apply: this.rBool() ? 'desaturate' : 'saturate', params: [this.rInt(40, 80)] },
+      { apply: 'spin', params: [this.rInt(10, 350)] }
     ]
+    if(special) {
+      if(msg.saturate !== undefined) filters.push({
+        apply: msg.saturate ? 'saturate' : 'desaturate',
+        params: [msg.saturateAmount || this.rInt(40, 80)]
+      })
+      if(msg.spin !== undefined) filters.push({ apply: 'spin', params: [msg.spin] })
+    }
     img1.color(filters)
     let img2 = await this.jimpToIM(img1)
     let horizRoll = this.rInt(0, img1.bitmap.width),
         vertiRoll = this.rInt(0, img1.bitmap.height)
-    if(msg.implode !== undefined && msg.implode !== 0) img2.out('-implode').out(`-${this.rInt(3, 10)}`)
-    if(msg.implode === undefined) {
+    if(!special) {
       img2.out('-implode').out(`-${this.rInt(3, 10)}`)
-    } else if(msg.implode) {
-      img2.out('-implode').out(`-${msg.implode}`)
-    }
-    if(msg.roll === undefined || msg.roll) img2.out('-roll').out(`+${horizRoll}+${vertiRoll}`)
-    if(msg.swirl === undefined) {
+      img2.out('-roll').out(`+${horizRoll}+${vertiRoll}`)
       img2.out('-swirl').out(`${this.rBool() ? '+' : '-'}${this.rInt(120, 180)}`)
-    } else if(msg.swirl) {
-      img2.out('-swirl').out(`${(msg.swirl < 0) ? '+' : '-'}${msg.swirl}`)
+    } else {
+      if(msg.implode !== undefined) img2.out('-implode').out(`-${msg.implode}`)
+      if(msg.swirl !== undefined) img2.out('-swirl').out(`${(msg.swirl < 0) ? '+' : '-'}${msg.swirl}`)
+      if(msg.roll) img2.out('-roll').out(`+${msg.horizontalRoll || 0}+${msg.verticalRoll || 0}`)
     }
+    console.log(msg, special, filters)
 
     this.sendIM(msg, img2)
   }
